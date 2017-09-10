becd619703c6a5dc28f82d971479333d5709e415
£ukasz Piecha <luki.piecha@gmail.com>
2017-09-10 16:53:20 +0200

    commit

--- warsztaty/warsztaty.js
+++ warsztaty/warsztaty.js
@@ -1,2 +1,35 @@
 'use strict';
 
+//  function ustawTlo() {
+//   
+//      document.getElementById('zmiana-koloru-p1').style.backgroundColor = "red";
+//      document.getElementById('zmiana-koloru-p2').style.backgroundColor = "yellow";
+//     
+//  }
+// 
+//document.getElementById('guzik').onclick = ustawTlo;
+//
+//document.getElementById("formularz").addEventListener("click", function(event){
+//    event.preventDefault()
+//});
+
+var fform = document.getElementById('formularz');
+
+function daneFormularza(e) {
+    e.preventDefault();
+    console.log('działa');
+    
+    var imie = document.getElementById('formularz').childNodes[1].value;
+    var nazwisko = document.getElementById('formularz').childNodes[5].value;
+    
+    console.log(imie);
+    console.log(nazwisko);
+}
+
+fform.onsubmit = daneFormularza;
+
+//console.log(document.getElementById)("fomularz").childNodes;
+//
+//console.log('--------------------------------')
+//console.log(document.getElementsByTagName('fname','lname'));
+
