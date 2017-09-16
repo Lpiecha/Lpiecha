3f732a434de6c83db492347422c0723a124439ba
£ukasz Piecha <luki.piecha@gmail.com>
2017-09-16 13:31:11 +0200

    commit

--- portfolio/jQuery/jQuery.js
+++ portfolio/jQuery/jQuery.js
@@ -1,27 +1,91 @@
 'use strict'
 
 
-$(function() {
-    $('h1')
-    .css({'color':'red'})
+//$(function() {
+//    $('h1')
+//    .css({'color':'red'})
+//    
+//    var allElements = $('p');
+//    console.log($('p'));
+//    
+//    var elementClass = $('p.pSecond');
+//    console.log ($('p.pSecond'));
+//    
+//    var idElement = $('#pFirst');
+//    console.log($('#pFirst'));
     
-    var allElements = $('p');
-    console.log($('p'));
-    
-    var elementClass = $('p.pSecond');
-    console.log ($('p.pSecond'));
-    
-    var idElement = $('#pFirst');
-    console.log($('#pFirst'));
-    
-    var tekstParagrafuPierwszego =
-    $("p.pFirst").text();
-    console.log($("tekstParagrafuPierwszego"));
-                
-    $("p.pSecond").append(" <b>Hello world!</b>");
-    
-    
-    
-    
-});
+//    var tekstParagrafuPierwszego =
+//    $("p.pFirst").text();
+//    console.log($("tekstParagrafuPierwszego"));
+//                
+//    $("p.pSecond").append(" <b>Hello world!</b>");
+//    
+//    $( "div" ).find( ".green" ).css( { "color": "red", "border-color": "red" } );
+//    
+//    $("iteracja").each( function() {
+//    var wszystkieIteracje = $( ".iteracja" );
+//    $("p.iteracja").append(" <b>Hello world!</b>");
+//    });
+//        $( "li" ).each(function( index ) {
+//        console.log( index + ": " + $( this ).text() );
+//});
+//    
+//    
+//    $('p').each(function(index)){
+//                
+//    console.log(index);
+//    
+//    $(this).text("paragraf " + (index+1));
+//    
+//}
+//  $(function() {
+//  $('#test2').on( {
+//    'click': function() {
+//        $("#test2").css("background-color","yellow"); 
+//        
+//    }}};
+                 
+//$("#test2").click(function(){
+//    $("test2").css("color", "red");
+//});  
+//
+//});
 
+//$(document).ready(function(){
+//    $("p").on( {
+//    'click': function(){
+//        $("#test2").css("color", "red"); 
+//    }});
+//}
+//
+$("mouseover").on({
+    mouseenter:function(){
+        $(".hideShow").show(3000);
+    },
+    mouseleave:function(){
+        $(".hideShow").hide(3000);
+    })
+});  
+//                  
+//.hideShow{
+//
+//.show(3000)
+//
+//.hide(3000)
+//}
+//        
+//        
+//$( "#test3" ).click( function() {
+//$( "p" ).fadeOut( "slow" );
+//} ); 
+//        
+//        
+        
+        
+        
+        
+        
+        
+        
+        
+        
