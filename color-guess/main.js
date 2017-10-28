41ba6ee4067710016a6b9231e94c7a0b1a40792b
£ukasz Piecha <luki.piecha@gmail.com>
2017-10-29 00:10:22 +0200

    commit

--- color-guess/main.js
+++ color-guess/main.js
@@ -1,58 +1,108 @@
 'use strict';
-
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var msgDisplay = document.querySelector("#msg");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function () {
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function () {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++) {
+       squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+});
+
+resetButton.addEventListener("click", function () {
+
+    colors = generateRandomColors(numSquares);
+
+    pickedColor = pickColor();
+
+    colorDisplay.textContent = pickedColor;
+
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "steelblue";
+})
+
+
+
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < squares.length; i++){
-    
+for (var i = 0; i < squares.length; i++) {
+
     squares[i].style.backgroundColor = colors[i];
-    
-    squares[i].addEventListener("click", function(){
-        
-    var clickedColor = this.style.backgroundColor;
-    
-    if(clickedColor === pickedColor){
-        msgDisplay.textContent = "Correct!"
-        changeColors(clickedColor);
-        h1.style.backgroundColor = clickedColor;
-    } else {
-        this.style.backgroundColor = "#131313";
-        msgDisplay.textContent = "Try again!"
-    }
-        
+
+    squares[i].addEventListener("click", function () {
+
+        var clickedColor = this.style.backgroundColor;
+
+        if (clickedColor === pickedColor) {
+            msgDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play again?"
+            changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
+        } else {
+            this.style.backgroundColor = "#131313";
+            msgDisplay.textContent = "Try again!"
+        }
+
     });
 }
 
-function changeColors(color){
-    
-    for(var i = 0; i < squares.length; i++){
+function changeColors(color) {
+
+    for (var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = color;
     }
 }
 
-function pickColor(){
-   var random = Math.floor(Math.random() * colors.length);
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
     return colors[random];
 }
 
-function generateRandomColors(num){
+function generateRandomColors(num) {
     var arr = []
-    for(var i = 0; i < num; i++){
-     arr.push(randomColor())   
+    for (var i = 0; i < num; i++) {
+        arr.push(randomColor())
     }
-    
+
     return arr;
 }
 
-function randomColor(){
+function randomColor() {
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
-    return   "rgb(" + r + ", " + g +", " + b + ")";
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
