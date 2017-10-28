c107598f04631e64838a11e76aebd0abc1bb3595
£ukasz Piecha <luki.piecha@gmail.com>
2017-10-28 22:58:22 +0200

    commit

--- color-guess/main.js
+++ color-guess/main.js
@@ -1,20 +1,12 @@
 'use strict';
 
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-
-var pickedColor = colors[3];
-
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
-
+var msgDisplay = document.querySelector("#msg");
+var h1 = document.querySelector("h1");
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
@@ -26,10 +18,41 @@ for(var i = 0; i < squares.length; i++){
     var clickedColor = this.style.backgroundColor;
     
     if(clickedColor === pickedColor){
-        alert("Correct!");
+        msgDisplay.textContent = "Correct!"
+        changeColors(clickedColor);
+        h1.style.backgroundColor = clickedColor;
     } else {
-        alert("Wrong!");
+        this.style.backgroundColor = "#131313";
+        msgDisplay.textContent = "Try again!"
     }
         
     });
+}
+
+function changeColors(color){
+    
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+   var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num){
+    var arr = []
+    for(var i = 0; i < num; i++){
+     arr.push(randomColor())   
+    }
+    
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return   "rgb(" + r + ", " + g +", " + b + ")";
 }
