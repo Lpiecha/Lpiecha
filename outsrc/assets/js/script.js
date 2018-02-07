531c2165111db561dd07e30409147ae8c0ea1d9d
£ukasz Piecha <luki.piecha@gmail.com>
2018-02-07 18:09:21 +0100

    forms

---
+++ outsrc/assets/js/script.js
@@ -0,0 +1,12 @@
+ 'use strict';
+
+$(function() {
+    
+	$("#right-arrow-container>a img").on("click", function() {
+	$("#slider-span").remove();
+        $("h4").remove();
+        $("h4").append
+    $("#slider-span").append("Milena Łuczak");
+    
+    });
+});
