7fc0e2cfcc3afd08f18770add217c3bd9acb62c6
£ukasz Piecha <luki.piecha@gmail.com>
2018-02-08 19:47:32 +0100

    slider

--- outsrc/assets/js/script.js
+++ outsrc/assets/js/script.js
@@ -1,12 +1,22 @@
  'use strict';
 
 $(function() {
+
+
+$(function () {
+
+    $("#right-arrow-container>a img").on("click", function () {
+        $("#slider-span").remove();
+        $("h4").remove();
+        $("h4").append("lorem lorem lorem");
+        $("#slider-span").append("Milena Łuczak");
+    });
     
-	$("#right-arrow-container>a img").on("click", function() {
+	$("#left-arrow-container>a img").on("click", function() {
 	$("#slider-span").remove();
         $("h4").remove();
-        $("h4").append
-    $("#slider-span").append("Milena Łuczak");
+        $("h4").append("lorem lorem lorem");
+    $("#slider-span").append("Jan Kowalski");
     
     });
 });
